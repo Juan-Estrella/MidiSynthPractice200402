555126bdd2edbf100bdc2cf8864a814ad1b856b3
Juan-Estrella <theenlightenedjuan@gmail.com>
2020-04-30 17:29:44 -0400

    Added math function to LFO

--- src/LfoUpdate.cpp
+++ src/LfoUpdate.cpp
@@ -10,14 +10,13 @@
 
 void Martone::LfoUpdate(bool retrig, u8 mode, float FILtop, float FILbottom, u8 m_str, u8 m_osc)
 {
-  //Serial.println("lfoUpdate");
-  static float localLFO[m_NUM_STRINGS-1][m_NUM_OSC-1] = {0};
-  static unsigned long lfoTime= {0};
+  static double localLFO[][m_NUM_OSC] = {0};
+  static unsigned long lfoTime[][m_NUM_OSC]= {0};
   static bool lfoDirection[m_NUM_STRINGS][m_NUM_OSC] = {false};
   unsigned long currentMicros;
   static bool lfoStop[m_NUM_STRINGS][m_NUM_OSC] = {false};
   static bool retriggered = false;
-  static float x = 0;
+  static  double x = 0;
   // bool activeLFO[NUM_VOICES] = {false};
   // int voice;
   // for(int i = 1;i<NUM_VOICES+1;i++)
@@ -30,35 +29,36 @@ void Martone::LfoUpdate(bool retrig, u8 mode, float FILtop, float FILbottom, u8 
   // //===============================================
  
 
-  if (currentMicros - lfoTime >= 4000 )//str[m_str].m_lfoSpeed[m_osc])
+  if (currentMicros - lfoTime[m_str][m_osc] >= str[m_str].m_lfoSpeed[m_osc] )
   {
-    lfoTime = currentMicros;
-   // Serial.println(lfoTime);
-   // delay(100);
-    str[m_str].m_lfoRange[m_osc] = 1;//FILtop - FILbottom;
+    lfoTime[m_str][m_osc] = currentMicros;
+    str[m_str].m_lfoRange[m_osc] = FILtop - FILbottom;
     if (str[m_str].m_lfoRange[m_osc] < 0) str[m_str].m_lfoRange[m_osc] = 0;
   //   //****************LFO*******Down then Up*************************************
                      //LFO value gets incremented here.
 
-    if (lfoDirection[m_str][m_osc] == true)  //Down
-    {
-      localLFO[m_str][m_osc] = (localLFO[m_str][m_osc] - 0.01);
-      if (localLFO[m_str][m_osc] <= 0)
-      {
-        lfoDirection[m_str][m_osc] = false;
-        lfoStop[m_str][m_osc] = true;
-      }
-    }
-    if (lfoDirection[m_str][m_osc] == false)  //UP
-    {
-      localLFO[m_str][m_osc] = (localLFO[m_str][m_osc] + 0.01);
-      if (localLFO[m_str][m_osc] >= 1)
-      {
-        lfoDirection[m_str][m_osc] = true;
-        lfoStop[m_str][m_osc] = true;
-        //localLFO[2] = 0;   //uncommenting this makes it go down, then up up up repeating.
-      }
-    }
+    // if (lfoDirection[m_str][m_osc] == true)  //Down
+    // {
+    //   //localLFO[m_str][m_osc] = (localLFO[m_str][m_osc] - 0.01);
+    //   localLFO[m_str][m_osc] =  MathFunctions(2, 1, 1.55, (x++)*.01);
+    //   if (localLFO[m_str][m_osc] <= 0)
+    //   {
+    //     lfoDirection[m_str][m_osc] = false;
+    //     lfoStop[m_str][m_osc] = true;
+    //   }
+    // }
+    //  if (lfoDirection[m_str][m_osc] == false)  //UP
+    //  {
+    //   //localLFO[m_str][m_osc] = (localLFO[m_str][m_osc] + 0.01);
+      localLFO[m_str][m_osc] =  MathFunctions(3, 1, str[m_str].m_filtSlope[m_osc], (x++)*.01);
+    //   if (localLFO[m_str][m_osc] >= 1)
+    //   {
+    //     //x = 0;
+    //     lfoDirection[m_str][m_osc] = true;
+    //     lfoStop[m_str][m_osc] = true;
+    //     //localLFO[2] = 0;   //uncommenting this makes it go down, then up up up repeating.
+    //   }
+    //  }
   //   //**********************************************************
   //   // LFO Modes
     switch (mode)
@@ -71,7 +71,7 @@ void Martone::LfoUpdate(bool retrig, u8 mode, float FILtop, float FILbottom, u8 
         {
           Serial.println("retriggered");
           lfoDirection[m_str][m_osc] = false;
-          localLFO[m_str][m_osc] = 0;
+          x = 0;
           // lfoDirection[m_str][m_osc] = true;
           // localLFO[m_str][m_osc] = 1.0;
         }
@@ -81,7 +81,7 @@ void Martone::LfoUpdate(bool retrig, u8 mode, float FILtop, float FILbottom, u8 
   //         {
   //           activeLFO[2] = true;
             str[m_str].m_lfo[m_osc] = localLFO[m_str][m_osc];
-            filtosc1a.frequency(10000 * (1*localLFO[m_str][m_osc]) + 0);
+            filtosc1a.frequency(str[m_str].m_freqCut[m_osc] * (str[m_str].m_lfoRange[m_osc] * localLFO[m_str][m_osc]) + str[m_str].m_lfoDepth[m_osc]);
  
 //Serial.println(localLFO[m_str][m_osc]);
 //delay(10);
